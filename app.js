b6e63105736e4e22848119e3d502a59838647e8a
puneetsingh99 <puneetsingh@github.com>
2020-12-03 19:13:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var input = document.querySelector("#txt-input");
+var inputText = input.value;
+
+var output = document.querySelector("#txt-output");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+var url = serverUrl + "?"+ "text=" + inputText;
+
+function error(err){
+    console.log("error: ", err);
+    console.log("something went wrong, please refresh and try after sometime");
+}
+
+function translator(){
+    fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(error)
+}
+
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", translator);
